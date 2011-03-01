fe0f724abd8565a3ee0a74437a018aea8fa83254
Adge Denkers <adge.denkers@gmail.com>
2011-03-01 14:59:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Go6::Application.config.secret_token = '73c0ef21e94d85c4cd0527a738756c4fe528ba13087f823d80d60cc2c9f885e7bf2b58bbb9cc7d286abaa3c2b649a7d09707881e9fb1f9929563f65b3696b2a0'
